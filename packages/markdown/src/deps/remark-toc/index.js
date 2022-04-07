076e905460d559e2398f4b0fd0abd5192384d127
Igor Suvorov <hi@isuvorov.com>
2022-04-07 10:22:39 +0300

    fix: remark toc

--- packages/markdown/src/deps/remark-toc/index.js
+++ packages/markdown/src/deps/remark-toc/index.js
@@ -1,30 +1,19 @@
-'use strict'
-
-var util = require('../mdast-util-toc')
-
-module.exports = toc
+import { toc as util } from '../mdast-util-toc';
 
 function toc(options) {
-  var settings = options || {}
+  const settings = options || {};
 
-  return transformer
+  return transformer;
 
   function transformer(node) {
-    var result = util(
-      node,
-      Object.assign({}, settings, {
-        heading: settings.heading || 'toc|table[ -]of[ -]contents?'
-      })
-    )
+    const result = util(node, { ...settings, heading: settings.heading || 'toc|table[ -]of[ -]contents?' });
 
     if (result.index === null || result.index === -1 || !result.map) {
-      return
+      return;
     }
 
-    node.children = [].concat(
-      node.children.slice(0, result.index),
-      result.map,
-      node.children.slice(result.endIndex)
-    )
+    node.children = [].concat(node.children.slice(0, result.index), result.map, node.children.slice(result.endIndex));
   }
 }
+
+module.exports = toc;
